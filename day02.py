32fec31760a877bc9b3a79aba5c7f645a08fa0b2
Rosso <peter.rosso@bristol.ac.uk>
2022-01-02 23:15:20 +0000

    Updates

---
+++ day02.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+# Advent of Code 2022
+
+f = open("./input/day02.txt", 'r')
+l = [x.split(' ') for x in f.read().splitlines()]
+test_l = [["forward","5"],["down","5"],["forward", "8"],["up","3"],["down", "8"],
+["forward", "2"]]
+depth = 0
+pos = 0
+
+for move in l:
+    dir = move[0]
+    step = int(move[1])
+    if dir == 'forward':
+        pos += step
+    elif dir == 'down':
+        depth += step
+    elif dir == 'up':
+        depth -= step
+print(depth)
+print(pos)
+print(depth*pos)
+
+# Part 2
+depth = 0
+pos = 0
+aim = 0
+for move in l:
+    dir = move[0]
+    step = int(move[1])
+    if dir == 'forward':
+        pos += step
+        depth += aim * step
+    elif dir == 'down':
+        aim += step
+    elif dir == 'up':
+        aim -= step
+print(depth)
+print(pos)
+print(depth*pos)
