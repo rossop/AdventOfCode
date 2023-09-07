7f3d90ed6264a19f233c26b9ce07481624227149
rossop <peter.rosso@bristol.ac.uk>
2023-09-07 09:51:44 +0000

    Finished problems 9 using objects in 9b (Class Rope could be changed to make inheritance more straightforward)

--- 9b.py
+++ 9b.py
@@ -1,75 +1,128 @@
+def read_instructions_from_file(file_path):
+  with open(file_path) as file:
+    lines = [
+      (direction, int(steps))
+      for direction, steps in (line.split()
+                               for line in file.read().strip().split('\n'))
+    ]
+  return lines
+
+
 class Rope:
   '''
   Rope class contain the position of the Head and Tail
   '''
 
   def __init__(self):
-    with open('in/9.input') as file:
-      lines = [(line.split(' ')[0], int(line.split(' ')[1]))
-               for line in file.read().strip().split('\n')]
-
-      self.instructions = lines
-      self.head_pos = [0, 0]
-      self.tail_pos = [0, 0]
-      self.dir = {'R': (0, 1), 'L': (0, -1), 'U': (1, 0), 'D': (-1, 0)}
-      self.dr = 0
-      self.dc = 0
-      self.visited = set()
-      self.visited.add(tuple(self.tail_pos))
-
-    print(self.instructions)
+    self.part = 1
+    self.instructions = read_instructions_from_file('in/9.input')
+    self.head_pos = [0, 0]
+    self.tail_pos = [0, 0]
+    self.dir = {'R': (0, 1), 'L': (0, -1), 'U': (1, 0), 'D': (-1, 0)}
+    self.dr = 0
+    self.dc = 0
+    self.visited = set()
+    self.visited.add(tuple(self.tail_pos))
 
   def __repr__(self):
-    return self.head_pos, self.tail_pos
+    return f"Part {self.part}: Head Position: {self.head_pos}, Tail Position: {self.tail_pos} which visited {len(self.visited)} positions"
 
   def move_head(self, move):
-    direction = self.dir[move[0]]
+    direction, steps = move
+    dx, dy = self.dir[direction]
 
-    for ii in range(move[1]):
-      self.head_pos = [
-        self.head_pos[0] + direction[0], self.head_pos[1] + direction[1]
-      ]
-      self.tail_distance()
-      if abs(self.dr) > 1 or abs(self.dc) > 1:
-        self.move_tail()
+    for _ in range(steps):
+      self.head_pos[0] += dx
+      self.head_pos[1] += dy
+      self.update_tail_distance()
+      self.move_tail_if_needed()
+
+  def move_tail_if_needed(self):
+    if abs(self.dr) > 1 or abs(self.dc) > 1:
+      self.move_tail()
 
   def move_tail(self):
-    if abs(self.dr) > 1:
-      if self.dc == 0:  # move vertically
-        self.tail_pos[0] = self.tail_pos[0] + self.dr / abs(self.dr)
-      else:  # move diagonally
-        self.tail_pos = [
-          self.tail_pos[0] + self.dr / abs(self.dr),
-          self.tail_pos[1] + self.dc / abs(self.dc)
-        ]
-
-    if abs(self.dc) > 1:
-      if self.dr == 0:  # move horizontally
-        self.tail_pos[1] = self.tail_pos[1] + self.dc / abs(self.dc)
-        pass
-      else:  # move diagonally
-        self.tail_pos = [
-          self.tail_pos[0] + self.dr / abs(self.dr),
-          self.tail_pos[1] + self.dc / abs(self.dc)
-        ]
+    '''
+    
+    '''
+    if self.dc == 0:  # move vertically
+      self.tail_pos[0] += self.dr // abs(self.dr)
+    elif self.dr == 0:  # move horizontally
+      self.tail_pos[1] += self.dc // abs(self.dc)
+    else:  # move diagonally
+      self.tail_pos = [
+        self.tail_pos[0] + self.dr // abs(self.dr),
+        self.tail_pos[1] + self.dc // abs(self.dc)
+      ]
 
     self.visited.add(tuple(self.tail_pos))
 
-  def tail_distance(self):
+  def update_tail_distance(self):
     self.dr = self.head_pos[0] - self.tail_pos[0]
     self.dc = self.head_pos[1] - self.tail_pos[1]
-
-  def is_tail_adjacent(self):
-    pass
 
   def start(self):
     for instruction in self.instructions:
       self.move_head(instruction)
 
+
 class LongRope(Rope):
-  def __
 
-R = Rope()
-R.start()
-print(len(R.visited))
+  def __init__(self):
+    super().__init__()
+    self.part = 2
+    self.number_of_knots = 10
+    self.tail_pos = [[0, 0] for _ in range(self.number_of_knots - 1)]
+    self.dr = [0 for _ in range(len(self.tail_pos))]
+    self.dc = [0 for _ in range(len(self.tail_pos))]
+    self.knots_pos = [self.head_pos] + self.tail_pos
+    # all knots start on 0,0 so there is no need in changing self.visited
 
+  def __repr__(self):
+    self.tail_pos = self.knots_pos[1:]
+    self.head_pos = self.knots_pos[0]
+    return super().__repr__()
+
+  def move_head(self, move):
+    direction, steps = move
+    dx, dy = self.dir[direction]
+
+    for _ in range(steps):
+      self.knots_pos[0][0] += dx
+      self.knots_pos[0][1] += dy
+      self.move_tail()
+
+  def move_tail(self):
+    '''
+    
+    '''
+    for ii in range(len(self.knots_pos) - 1):
+      self.update_tail_distance(ii)
+      if abs(self.dr[ii]) > 1 or abs(self.dc[ii]) > 1:
+        if self.dc[ii] == 0:  # move vertically
+          self.knots_pos[ii + 1][0] += self.dr[ii] // abs(self.dr[ii])
+        elif self.dr[ii] == 0:  # move horizontally
+          self.knots_pos[ii + 1][1] += self.dc[ii] // abs(self.dc[ii])
+        else:  # move diagonally
+          self.knots_pos[ii + 1] = [
+            self.knots_pos[ii + 1][0] + self.dr[ii] // abs(self.dr[ii]),
+            self.knots_pos[ii + 1][1] + self.dc[ii] // abs(self.dc[ii])
+          ]
+      # else:
+      #   break
+
+    self.visited.add(tuple(self.knots_pos[-1]))
+
+  def update_tail_distance(self,ii):
+      self.dr[ii] = self.knots_pos[ii][0] - self.knots_pos[ii + 1][0]
+      self.dc[ii] = self.knots_pos[ii][1] - self.knots_pos[ii + 1][1]
+
+
+if __name__ == "__main__":
+  rope = Rope()
+  rope.start()
+  print(rope)
+
+  long_rope = LongRope()
+  long_rope.start()
+  print(long_rope)
