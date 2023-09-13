f827372fe45ac96239071768c10161c0a9326c8e
rossop <peter.rosso@bristol.ac.uk>
2023-09-13 10:37:31 +0000

    Changed submission in object to easily reuse aspects of part one

--- 8.py
+++ 8.py
@@ -11,80 +11,82 @@ def read_input(filename):
     forest = [list(map(int, line)) for line in file.read().strip().split('\n')]
   return forest
 
+class Forest():
+  def __init__(self,data):
+    self.forest = data
+    # check trees from all cardinal points
+    self.R = len(self.forest)
+    self.C = len(self.forest[0])
+    self.R_edge = [0, self.R - 1]
+    self.C_edge = [0, self.C - 1]
 
-def part_one(forest):
-  # Your code for part one goes here
+    # Initialize visibility_grid with True at borders and False inside
+    self.visibility_grid = [[
+      False if r != 0 and c != 0 and r != self.R - 1 and c != self.C - 1 else True
+      for c in range(self.C)
+    ] for r in range(self.R)]
 
-  # check trees from all cardinal points
-  R = len(forest)
-  C = len(forest[0])
-  R_edge = [0, R - 1]
-  C_edge = [0, C - 1]
-
-
-  # Initialize visibility_grid with False at borders and True inside
-  visibility_grid = [[
-    False if r != 0 and c != 0 and r != R - 1 and c != C - 1 else True
-    for c in range(C)
-  ] for r in range(R)]
-
-  # Check from the top and bottom borders
-  for r in range(R):
-    val = forest[r][0]
-    for c in range(1, C):
-      if forest[r][c] > val:
-        val = forest[r][c]
-        if (r not in R_edge) and (c not in C_edge):
-          visibility_grid[r][c] = True
-      
-
-    val = forest[r][-1]
-    for c in range(C - 2, -1, -1):
-      if forest[r][c] > val:
-        val = forest[r][c]
-        if (r not in R_edge) and (c not in C_edge):
-          visibility_grid[r][c] = True
-      
-
-  # Check from the left and right borders
-  for c in range(C):
-    val = forest[0][c]
-    for r in range(1, R):
-      if forest[r][c] > val:
-        val = forest[r][c]
-        if (r not in R_edge) and (c not in C_edge):
-          visibility_grid[r][c] = True
-      
-
-    val = forest[-1][c]
-    for r in range(R - 2, -1, -1):
-      if forest[r][c] > val:
-        val = forest[r][c]
-        if (r not in R_edge) and (c not in C_edge):
-          visibility_grid[r][c] = True
-      
-
-  # Count the number of not visible points
-  visible = sum(
-    sum(1 for val in row if val) for row in visibility_grid)
-  # Calculate the number of visible points
-
-  return visible
+  
+  def part_one(self):
+    # Check from the top and bottom borders
+    for r in range(self.R):
+      val = self.forest[r][0]
+      for c in range(1, self.C):
+        if self.forest[r][c] > val:
+          val = self.forest[r][c]
+          if (r not in self.R_edge) and (c not in self.C_edge):
+            self.visibility_grid[r][c] = True
+        
+  
+      val = self.forest[r][-1]
+      for c in range(self.C - 2, -1, -1):
+        if self.forest[r][c] > val:
+          val = self.forest[r][c]
+          if (r not in self.R_edge) and (c not in self.C_edge):
+            self.visibility_grid[r][c] = True
+        
+  
+    # Check from the left and right borders
+    for c in range(self.C):
+      val = self.forest[0][c]
+      for r in range(1, self.R):
+        if self.forest[r][c] > val:
+          val = self.forest[r][c]
+          if (r not in self.R_edge) and (c not in self.C_edge):
+            self.visibility_grid[r][c] = True
+        
+  
+      val = self.forest[-1][c]
+      for r in range(self.R - 2, -1, -1):
+        if self.forest[r][c] > val:
+          val = self.forest[r][c]
+          if (r not in self.R_edge) and (c not in self.C_edge):
+            self.visibility_grid[r][c] = True
+        
+  
+    # Count the number of not visible points
+    visible = sum(
+      sum(1 for val in row if val) for row in self.visibility_grid)
+    # Calculate the number of visible points
+  
+    return visible
 
 
-def part_two(data):
-  # Your code for part two goes here
-  result = 0
-  return result
+  def part_two(self):
+    # Your code for part two goes here
+    
+    result = 0
+    return result
 
 
 def main():
   input_data = read_input("in/8.in")
 
-  result_one = part_one(input_data)
+  f = Forest(input_data)
+  result_one = f.part_one()
   print(f"Part One: {result_one}")
 
-  result_two = part_two(input_data)
+  result_two = f.part_two()
   print(f"Part Two: {result_two}")
 
 
