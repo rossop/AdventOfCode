d83f9b3d38d4c6878431d1922e4e9f88026b2d19
rossop <peter.rosso@bristol.ac.uk>
2023-09-13 10:20:33 +0000

    Day 8 Part I

--- 8.py
+++ 8.py
@@ -1,92 +1,92 @@
+# Advent of Code
+from copy import deepcopy
+from collections import deque
 import pprint
 
 pp = pprint.PrettyPrinter(indent=4)
 
-with open('in/8.test') as file:
-  forest = [list(map(int, line)) for line in file.read().strip().split('\n')]
 
-# check treess from all cardinal points
-R = len(forest)
-C = len(forest[0])
+def read_input(filename):
+  with open(filename) as file:
+    forest = [list(map(int, line)) for line in file.read().strip().split('\n')]
+  return forest
 
-visibility_grid = [[
-  False if r == 0 or c == 0 or r == R - 1 or c == C - 1 else True
-  for r in range(R)
-] for c in range(C)]
 
-# directions = [(dr, dc) for dr in [-1, 0, 1] for dc in [-1, 0, 1]
-#               if not ((dr + dc) in [0, 2, -2])] # exclude center and diagonals
-directions = [(-1, 0), (0, -1), (0, 1), (1, 0)]
+def part_one(forest):
+  # Your code for part one goes here
 
-for r in range(R):
-  val = forest[r][0]
-  for c in range(1, C):
-    if forest[r][c] > val:
-      val = forest[r][c]
-      visibility_grid[r][c] = False
-    else:
-      visibility_grid[r][c] = visibility_grid[r][c] and True
-  val = forest[r][-1]
-  for c in range(C - 2, -1, -1):
-    if forest[r][c] > val:
-      val = forest[r][c]
-      visibility_grid[r][c] = False
-    else:
-      visibility_grid[r][c] = visibility_grid[r][c] and True
+  # check trees from all cardinal points
+  R = len(forest)
+  C = len(forest[0])
+  R_edge = [0, R - 1]
+  C_edge = [0, C - 1]
 
-for c in range(C):
-  val = forest[0][c]
-  for c in range(1, R):
-    if forest[r][c] >= val:
-      val = forest[r][c]
-      visibility_grid[r][c] = False
-    else:
-      visibility_grid[r][c] = visibility_grid[r][c] and True
-  val = forest[-1][c]
-  for c in range(R - 2, -1, -1):
-    if forest[r][c] >= val:
-      val = forest[r][c]
-      visibility_grid[r][c] = False
-    else:
-      visibility_grid[r][c] = visibility_grid[r][c] and True
 
-visible = sum(sum(1 for val in row if not val) for row in visibility_grid)
+  # Initialize visibility_grid with False at borders and True inside
+  visibility_grid = [[
+    False if r != 0 and c != 0 and r != R - 1 and c != C - 1 else True
+    for c in range(C)
+  ] for r in range(R)]
 
-pp.pprint(visibility_grid)
-print(visible)
-# The code below was wrong because I was not checking the whole row, but only neighbouring trees
+  # Check from the top and bottom borders
+  for r in range(R):
+    val = forest[r][0]
+    for c in range(1, C):
+      if forest[r][c] > val:
+        val = forest[r][c]
+        if (r not in R_edge) and (c not in C_edge):
+          visibility_grid[r][c] = True
+      
 
-def is_visible(forest, pos):
-  '''
-  given a forest and a specific position calculate if that tree is
-  visible from the outside
-  '''
-  neighbours = [(pos[0] + dr, pos[1] + dc) for dr in [-1, 0, 1]
-                for dc in [-1, 0, 1] if not (dr == 0 and dc == 0)]
-  visible = False
-  for r, c in neighbours:
-    visible = (forest[pos[0]][pos[1]] <) or visible
+    val = forest[r][-1]
+    for c in range(C - 2, -1, -1):
+      if forest[r][c] > val:
+        val = forest[r][c]
+        if (r not in R_edge) and (c not in C_edge):
+          visibility_grid[r][c] = True
+      
+
+  # Check from the left and right borders
+  for c in range(C):
+    val = forest[0][c]
+    for r in range(1, R):
+      if forest[r][c] > val:
+        val = forest[r][c]
+        if (r not in R_edge) and (c not in C_edge):
+          visibility_grid[r][c] = True
+      
+
+    val = forest[-1][c]
+    for r in range(R - 2, -1, -1):
+      if forest[r][c] > val:
+        val = forest[r][c]
+        if (r not in R_edge) and (c not in C_edge):
+          visibility_grid[r][c] = True
+      
+
+  # Count the number of not visible points
+  visible = sum(
+    sum(1 for val in row if val) for row in visibility_grid)
+  # Calculate the number of visible points
 
   return visible
 
-def count_visible(forest):
-  '''
-  take the forest as an input and count outbounding and inside
-  visible trees
-  '''
-  R = len(forest)
-  C = len(forest[0])
 
-  visible_counter = 2 * R + 2 * (C - 2)  # borders
-  print(visible_counter)
+def part_two(data):
+  # Your code for part two goes here
+  result = 0
+  return result
 
-  for r in range(1, R - 1):  # exclude border
-    for c in range(1, C - 1):
 
-      if is_visible(forest, [r][c]):
-        visible_counter += 1
-        print(forest[r][c])
+def main():
+  input_data = read_input("in/8.in")
 
-  return visible_counter
+  result_one = part_one(input_data)
+  print(f"Part One: {result_one}")
 
-print(count_visible(forest))
+  result_two = part_two(input_data)
+  print(f"Part Two: {result_two}")
+
+
+if __name__ == "__main__":
+  main()
