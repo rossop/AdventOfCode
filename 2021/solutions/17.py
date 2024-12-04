eb73df07a032ad07ce39ed19f483b18c03c390af
Rosso <25611971+rossop@users.noreply.github.com>
2024-12-04 23:19:28 +0100

    sol: solve day17 2021 pt1

---
+++ 2021/solutions/17.py
@@ -0,0 +1,98 @@
+"""
+Template for Advent of Code solutions (located in YYYY/solutions).
+
+This file provides a structure for solving Advent of Code challenges.
+The input files are expected to be located in the 'YYYY/in' directory.
+"""
+
+import os
+import re
+import sys
+from pathlib import Path
+from typing import Any, Dict, List, Optional, Set
+
+# Add the parent directory of 'utils' to sys.path
+sys.path.append(str(Path(__file__).resolve().parent.parent.parent))
+
+import utils
+
+input_directory: str = os.path.join(
+    os.path.dirname(os.path.abspath(os.path.dirname(__file__))), "in"
+)
+
+
+def process(raw_data: str) -> Any:
+    """Processes the input data."""
+    pattern: str = r"-?\d+"
+    return list(map(int, re.findall(pattern, raw_data)))
+
+
+def steps_for_dy(dy, miny, maxy):
+    """move step vertically"""
+    y: int = 0
+    steps: int = 0
+    valid: List[int] = []
+    while y >= miny:
+        if miny <= y <= maxy:
+            valid.append(steps)
+        y += dy
+        dy -= 1
+        steps += 1
+    return valid
+
+
+def can_land_dx(step: int, minx: int, maxx: int) -> bool:
+    """Check if it cal land on platform"""
+    for dx in range(1, maxx):
+        x: int = 0
+        for _ in range(step):
+            x += dx
+            if dx > 0:
+                dx -= 1
+        if minx <= x <= maxx:
+            return True
+    return False
+
+
+def solve_part_one(data: Any) -> Any:
+    """Solves part one of the challenge."""
+    if data is None:
+        return None
+    minx: int
+    maxx: int
+    miny: int
+    maxy: int
+    minx, maxx, miny, maxy = data
+
+    dy: int = -miny
+
+    answer_part_one: Optional[int] = None
+    while True:
+        if any(can_land_dx(step, minx, maxx) for step in steps_for_dy(dy, miny, maxy)):
+            answer_part_one = sum(range(1, dy + 1))
+            print(answer_part_one)
+            break
+        dy -= 1
+    return answer_part_one
+
+
+def solve_part_two(data: Any) -> Any:
+    """Solves part two of the challenge."""
+    # TODO: Implement the solution for part two
+    return None
+
+
+if __name__ == "__main__":
+    day: str = __file__.rsplit("/", maxsplit=1)[-1].replace(".py", "")
+    infile = sys.argv[1] if len(sys.argv) >= 2 else f"{day}.in"
+
+    file_path: str = os.path.join(input_directory, infile)
+    unprocessed_data = utils.read_input(file_path)
+    input_data = process(unprocessed_data["data"])
+
+    result_part_one = solve_part_one(input_data)
+    if result_part_one is not None:
+        print(f"Part One: {result_part_one}")
+    result_part_two = solve_part_two(input_data)
+    if result_part_two is not None:
+        print(f"Part Two: {result_part_two}")
